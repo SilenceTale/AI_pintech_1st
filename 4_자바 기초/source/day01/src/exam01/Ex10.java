24f30f9cfe84a960e4a8e7a3675c00c5c17b2c1b
SilenceTale <chomn55@gmail.com>
2024-10-07 16:53:21 +0900

    day01 수업 소스

---
+++ 4_자바 기초/source/day01/src/exam01/Ex10.java
@@ -0,0 +1,11 @@
+package exam01;
+
+public class Ex10 {
+    public static void main(String[] args) {
+        int num1 = 1000000000;
+        float num2 = num1; // int형태 자료형이 -> float 자료형으로 변환 : 자동 형변환 (정수 -> 실수)
+
+        long num3 = 100000000000L; // 8바이트
+        float num4 = num3; // long형태 자료형이 -> float로 변환
+    }
+}
