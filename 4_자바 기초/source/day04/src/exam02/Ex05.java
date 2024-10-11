56b8c9dc1f61fb822f94d11be1d6d38cc1020ff7
SilenceTale <chomn55@gmail.com>
2024-10-11 18:23:42 +0900

    Add files via upload

---
+++ 4_자바 기초/source/day04/src/exam02/Ex05.java
@@ -0,0 +1,21 @@
+package exam02;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public class Ex05 {
+    public static void main(String[] args) {
+        Class cls = Student.class;
+
+        System.out.println("--- 생성자---");
+        Constructor[] constructors = cls.getConstructors();
+        for (Constructor constructor : constructors) {
+            System.out.println(constructor);
+        }
+        Method[] methods = cls.getMethods();
+        for (Method method : methods)
+        {
+            System.out.println(method);
+        }
+    }
+}
