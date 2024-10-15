7c7b10a08e19278a3ce4df9c956a2a2c0e223c5b
SilenceTale <chomn55@gmail.com>
2024-10-15 11:50:05 +0900

    Add files via upload

---
+++ 4_자바 기초/source/day06/src/exam01/Ex01.java
@@ -0,0 +1,13 @@
+// A 클래스 상속받은 B 클래스 객체 사용
+
+package exam01;
+
+public class Ex01 {
+    public static void main(String[] args) {
+
+        B b = new B();
+
+        System.out.println(b.numA);
+        // 10
+    }
+}
