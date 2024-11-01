51fef9da54254ac2d31971c3679b658c7af81b80
SilenceTale <chomn55@gmail.com>
2024-11-01 15:40:48 +0900

    수업소스 2024-11-01

--- 6.스프링 기초/exam01/src/main/java/org/koreait/member/services/JoinService.java
+++ 6.스프링 기초/exam01/src/main/java/org/koreait/member/services/JoinService.java
@@ -2,13 +2,34 @@ package org.koreait.member.services;
 
 import org.koreait.global.validators.Validator;
 import org.koreait.member.controllers.RequestJoin;
-import org.koreait.member.validators.JoinValidator;
 
 /**
  * 회원 가입 기능
  */
 public class JoinService {
-    private Validator<RequestJoin> joinValidator = new JoinValidator();
+        //구성
+    private Validator<RequestJoin> joinValidator;
+
+    /**
+     * 의존 - 서로를 필요로 하는 고나계
+     * 의존 관계
+     * - JoinService 객체 생성을 위해서는 joinValidator가 필수! -> 이게 의존 관계.
+     * - JoinService 와 JoinValidator는 서로 의존하고 연관돼있다.
+     * @param joinValidator
+     */
+    public JoinService(Validator<RequestJoin> joinValidator) {
+        this.joinValidator = joinValidator;
+    }
+
+    /**
+     * 연관 관계.
+     * JoinService 객체를 생성할때 필수는 아니기떄문
+     * @param joinValidator
+     */
+
+//    public void setValidator(Validator<RequestJoin> joinValidator) {
+//        this.joinValidator = joinValidator;
+//    }
 
     /**
      * 가입 처리
