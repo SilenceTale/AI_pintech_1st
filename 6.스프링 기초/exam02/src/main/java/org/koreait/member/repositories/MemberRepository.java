e92abc16f9ef6ebe7ccf0d80566c3af4680c0569
SilenceTale <chomn55@gmail.com>
2024-11-04 11:03:22 +0900

    11-04 수업소스

---
+++ 6.스프링 기초/exam02/src/main/java/org/koreait/member/repositories/MemberRepository.java
@@ -0,0 +1,13 @@
+package org.koreait.member.repositories;
+
+import org.koreait.member.controllers.RequestJoin;
+/**
+ * 영구 저장 처리 또는 조회 또는 삭제 처리...
+ * DB에 접근하는 역할...
+ */
+public class MemberRepository {
+
+    public void register(RequestJoin form) {
+
+    }
+}
