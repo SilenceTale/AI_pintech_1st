96e6f6e50b3e86ff3f0b41c58761c95ead12013d
SilenceTale <chomn55@gmail.com>
2024-11-04 16:57:43 +0900

    수업 소스 2024-11-04

--- 4_자바 기초/day17/src/exam01/Book.java
+++ 4_자바 기초/day17/src/exam01/Book.java
@@ -1,8 +1,31 @@
 package exam01;
 
-public class Book {
+public class Book implements Comparable<Book> {
     private int isdn;
     private String title;
     private String author;
     private String publisher;
+
+    public Book(int isdn, String title, String author, String publisher) {
+        this.isdn = isdn;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isdn=" + isdn +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", publisher='" + publisher + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Book o) {
+        return isdn - o.isdn;
+    }
 }
