efe8bf7f4f8c2f4d451da730774cecad068a2e0f
SilenceTale <chomn55@gmail.com>
2024-11-05 16:10:32 +0900

    수업 소스 2024-11-05

--- 4_자바 기초/source/day18/src/exam01/Ex01.java
+++ 4_자바 기초/source/day18/src/exam01/Ex01.java
@@ -9,11 +9,13 @@ public class Ex01 {
         // 배열 형식
         String[] items1 = {"항목1", "항목1", "항목1", "항목2", "항목3", "항목4", "항목5"};
         List<String> cItems1 = Arrays.stream(items1).distinct().map(s -> String.format("**%s**", s)).toList();
+        System.out.println(items1); // 원 소스는 스트림에서 변경하지 않는다.
         System.out.println(cItems1);
 
         // 컬렉션 형식
         List<String> items2 = List.of("항목1", "항목1", "항목1", "항목2", "항목3", "항목4", "항목5");
         List<String> cItems2 = items2.stream().distinct().map(s -> String.format("**%s**", s)).toList();
+        System.out.println(items2); // 원 소스는 스트림에서 변경하지 않는다.
         System.out.println(cItems2);
     }
 }
