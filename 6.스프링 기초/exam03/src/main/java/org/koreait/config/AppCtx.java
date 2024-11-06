298f7369b2afd723873fd5797116eb7563fdbf01
SilenceTale <chomn55@gmail.com>
2024-11-06 12:23:49 +0900

    수업소스 2024-11-06

--- 6.스프링 기초/exam03/src/main/java/org/koreait/config/AppCtx.java
+++ 6.스프링 기초/exam03/src/main/java/org/koreait/config/AppCtx.java
@@ -5,6 +5,7 @@ import org.koreait.exam04.RecCalculator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.annotation.Order;
 
 @Configuration
 /**
@@ -12,8 +13,14 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
  * 자바 표준은 인터페이스 기반의 프록시이다.
  * 스프링 쪽에는 기능을 확장해서 서브클래스 기반 프록시도 구현!
  */
-@EnableAspectJAutoProxy(proxyTargetClass = true)
+//@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableAspectJAutoProxy
 public class AppCtx {
+
+    @Bean
+    public SpringCachedCalculator springCachedCalculator() {
+        return new SpringCachedCalculator();
+    }
 
     @Bean
     public SpringProxyCalculator springProxyCalculator() {
@@ -21,6 +28,7 @@ public class AppCtx {
     }
 
     @Bean
+
     public Calculator calculator() {
         return new RecCalculator();
     }
--- 6.스프링 기초/exam03/src/test/java/org/koreait/exam04/Ex01.java
+++ 6.스프링 기초/exam03/src/test/java/org/koreait/exam04/Ex01.java
@@ -1,13 +1,27 @@
 package org.koreait.exam04;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.koreait.config.AppCtx;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Ex01 {
+
+    private AnnotationConfigApplicationContext ctx;
+    @BeforeEach
+    void init() {
+        ctx = new AnnotationConfigApplicationContext(AppCtx.class);
+    }
+
+    @AfterEach
+    void close() {
+        ctx.close();
+    }
+
     @Test
-    void test1() {
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
+    void test1() { // 위에 BeforeEach를 정의했다면 test1에 ctx는 지워도 괜찮다!
+        //AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
 
         //RecCalculator cal = ctx.getBean(RecCalculator.class);
         Calculator cal = ctx.getBean(Calculator.class);
@@ -15,6 +29,20 @@ public class Ex01 {
         Long result = cal.factorial(10L);
         System.out.println(result);
 
-        ctx.close();
+        //ctx도 @AfterEach가 정의돼서 일을 함으로써 지워도 문제가 없다.
+        //ctx.close();
+    }
+
+    @Test
+    void test2() { //cache 가 돼는지 체크 용
+       Calculator cal = ctx.getBean(Calculator.class); // 캐시에 값을 저장
+       long r1 = cal.factorial(10L);
+        System.out.printf("r1= %d%n", r1);
+
+        long r2 = cal.factorial(10L); // 캐시에서 가져옴!
+        System.out.printf("r2= %d%n", r2);
+
+        long r3 = cal.factorial(10L); // 캐시에서 가져옴!
+        System.out.printf("r3= %d%n", r3);
     }
 }
