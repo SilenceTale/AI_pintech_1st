ea53b65f9ade64f97484ee306086b8a0d16a57f9
SilenceTale <chomn55@gmail.com>
2024-11-07 16:01:48 +0900

    수업소스 2024-11-07

--- 6.스프링 기초/exam04/src/main/java/org/koreait/member/entities/Member.java
+++ 6.스프링 기초/exam04/src/main/java/org/koreait/member/entities/Member.java
@@ -1,11 +1,13 @@
 package org.koreait.member.entities;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 import java.time.LocalDateTime;
 
 @Data
 public class Member {
+    @Id // 기본키로 사용할 컬럼
     private long seq;
     private String email;
     private String password;
