a4c2b00fd7359e778737b89c03a2e876f63c1f64
SilenceTale <chomn55@gmail.com>
2024-11-07 10:08:08 +0900

    수업소스 2024-11-07

--- 4_자바 기초/source/day19/src/exam01/Ex04.java
+++ 4_자바 기초/source/day19/src/exam01/Ex04.java
@@ -1,13 +1,4 @@
 package exam01;
 
-import java.util.Random;
-
 public class Ex04 {
-    public static void main(String[] args) {
-        /*Random random = new Random();
-        int odd = random.doubles().mapToInt(x -> (int)(x * 10000)).filter(x -> x % 2 == 1).findFirst().orElse(-1);
-        System.out.println(odd);*/
-        Random random = new random();
-        int odd = random.ints()
-    }
 }
