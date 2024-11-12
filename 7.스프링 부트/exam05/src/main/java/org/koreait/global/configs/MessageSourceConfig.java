3c249439726a7a5473fdad16fcfb2b53b28f2562
SilenceTale <chomn55@gmail.com>
2024-11-12 13:10:26 +0900

    수업 소스 2024-11-12

---
+++ 7.스프링 부트/exam05/src/main/java/org/koreait/global/configs/MessageSourceConfig.java
@@ -0,0 +1,19 @@
+package org.koreait.global.configs;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@Configuration
+public class MessageSourceConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+
+        ms.addBasenames("messages.commons"); //properties 파일만 사용가능.
+        ms.setDefaultEncoding("UTF-8"); // 디폴트 인코딩을 "UTF-8"로 설정함.
+        return ms;
+    }
+}
