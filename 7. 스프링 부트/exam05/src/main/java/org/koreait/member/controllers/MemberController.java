9101a63c3301d5258e08fbd5d16280b09fe311d2
SilenceTale <chomn55@gmail.com>
2024-11-14 18:13:57 +0900

    Add files via upload

---
+++ 7. 스프링 부트/exam05/src/main/java/org/koreait/member/controllers/MemberController.java
@@ -0,0 +1,117 @@
+package org.koreait.member.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.koreait.member.Services.LoginService;
+import org.koreait.member.validators.JoinValidator;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/member")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final JoinValidator joinValidator;
+    private final LoginService loginService;
+
+    /**
+     * 회원가입 양식
+     *
+     * @return
+     */
+//    @GetMapping("/join")
+//    public String join(Model model) {
+//
+//        RequestJoin requestJoin = new RequestJoin();
+//        model.addAttribute("requestJoin", requestJoin);
+//
+//        return "member/joinForm";
+//    }
+
+    @ModelAttribute("apples")
+    public List<String> apples() {
+        return List.of("사과1", "사과2", "사과3");
+    }
+
+    @GetMapping("/join")
+    public String join(@ModelAttribute RequestJoin form, Model model) { // RequestJoin - requestJoin
+
+
+        return "member/joinForm";
+    }
+
+    /**
+     * 회원 가입 처리!
+     * @Valid : 검증할 커맨드 객체임을 알려준다!
+     * @Valid 가 있으면 반드시 Errors 객체는 커맨드 객체 바로 다음에 나와야한다!
+     * @return
+     */
+    @PostMapping("/join")
+    public String joinPs(@Valid RequestJoin form, Errors errors) {
+
+        joinValidator.validate(form, errors); // 커맨드 객체 검증
+
+        if (errors.hasErrors()) { // 검증 실패! - reject, rejectValue가 한 번이라도 호출 되었다!
+            return "member/joinForm"; // 검증 실패하면 사용자에게 양식을 다시 보여주고, 검증 실패 정보를 제공
+        }
+
+        // 검증 성공시 - 가입 처리 서비스 호출
+
+        return "redirect:/member/login"; // 가입 성공시 로그인 페이지로 이동
+    }
+
+    @GetMapping("/login")
+    public String login(@ModelAttribute RequestLogin form, @CookieValue(name="SavedEmail",required = false)
+            String savedEmail) {
+
+        if (savedEmail != null) { // 쿠키값이 있다면
+            form.setEmail(savedEmail);
+            form.setSaveEmail(true);
+        }
+
+        return "member/login"; // 얘는 템플릿 경로이니 햇갈려하지 말것.
+    }
+
+    @PostMapping("/login")
+    public String loginPs(@Valid RequestLogin form, Errors errors) {
+
+        if (errors.hasErrors()) {
+            return "member/login";
+        }
+
+        // 검증에 이상이 없는 상태 -> 로그인 처리
+        loginService.process(form);
+
+
+        return "redirect:/";
+    }
+
+    @RequestMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate(); // 세션 비우기
+
+        return "redirect:/member/login";
+    }
+
+    /**
+     * MemberController 공통 검증
+     * @Valid가 붙어있는 커맨드 객체 공통 검증 처리
+     *
+     * @param binder
+     */
+    /*
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.setValidator(joinValidator);
+    } */
+}
